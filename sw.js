b679e3e2a48226df83f746eb86b2db67aa95e463
xaglobally-lgtm <xaglobally@gmail.com>
2026-09-20 14:35:16 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+var CACHE = 'foresite-v1';
+var ASSETS = ['./foresite.html', './manifest.webmanifest', './icon.svg'];
+
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function (c) {
+      return Promise.all(ASSETS.map(function (a) {
+        return c.add(a).catch(function () {});
+      }));
+    }).then(function () { return self.skipWaiting(); })
+  );
+});
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(keys.filter(function (k) { return k !== CACHE; }).map(function (k) { return caches.delete(k); }));
+    }).then(function () { return self.clients.claim(); })
+  );
+});
+
+self.addEventListener('fetch', function (e) {
+  if (e.request.method !== 'GET') return;
+  if (new URL(e.request.url).origin !== self.location.origin) return;
+  e.respondWith(
+    caches.match(e.request).then(function (cached) {
+      var fetched = fetch(e.request).then(function (resp) {
+        if (resp && resp.status === 200) {
+          var copy = resp.clone();
+          caches.open(CACHE).then(function (c) { c.put(e.request, copy); });
+        }
+        return resp;
+      }).catch(function () { return cached; });
+      return cached || fetched;
+    })
+  );
+});
